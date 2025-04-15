4f0ac4dc3625767f85fb9af3c949b1603534c530
MiguelAzuaga <miguel.q.azuaga@gmail.com>
2025-04-15 19:52:21 +0100

    added lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mqueiros <mqueiros@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 17:36:16 by mqueiros          #+#    #+#             */
-/*   Updated: 2025/04/15 17:45:14 by mqueiros         ###   ########.fr       */
+/*   Updated: 2025/04/15 19:49:49 by mqueiros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	tmp;
-	
-	tmp = *lst;
-	
-}
+	t_list	*tmp;
+	t_list	*dst;
 
+	if (!lst || !f || !del)
+		return (NULL);
+	dst = NULL;
+	while (lst)
+	{
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&dst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&dst, tmp);
+		lst = lst->next;
+	}
+	return (dst);
+}
